31620b103c9e9c09e1ebf6b03a289e9549c45bdc
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-03-19 19:41:27 +0300

    Final revision 1.1. Added by correct description.

--- numerated_lists/5x4.py
+++ numerated_lists/5x4.py
@@ -1,27 +1,34 @@
-# creating matrix m = 5x4
-m = [[0 for i in range(5)] for j in range(4)]
-# m = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-for i in m:
-    print(i)  # printing m
-x = -1  # initial position in the column (horisontal moving)
-y = 0  # initial position in the string (vertical moving)
-dx = 1  # moving in the column (horisontal moving):
-        # +1 - to the right
-        # 0 - no move
-        # -1 - to the left
-dy = 0  # moving in the string (vertical moving):
-        # +1 - down
-        # 0 - no move
-        # -1 - up
-n = 1
+''' Creating matrix (size = AxB),
+filled with numbers from 1 to matrix size (A*B). '''
 
-while n <= 20:
-    if 0 <= x + dx < len(m[0]) and 0 <= y + dy < len(m) and m[y + dy][x + dx] == 0:
+# Stage 1: creating matrix
+a = int(input('Enter horizontal matrix size: '))
+b = int(input('Enter vertical matrix size: '))
+m = [[0] * a for i in range(b)]
+# Creating initial conditions (position and direction)
+# for initial and further enumeration and filling of matrix
+# cells. Initial position is left-upper cell of matrix.
+x = -1
+y = 0
+dx = 1
+dy = 0
+n = 1
+# Implementation of control over the excess
+# of the upper value of the matrix content
+while n <= a * b:
+    # Implementation of control over feeling the free cells.
+    # Implementation of control over the formation
+    # of correct indexes of matrix cells during the moving.
+    if 0 <= x + dx < len(m[0]) and 0 <= y + dy < len(m) \
+            and m[y + dy][x + dx] == 0:
         x += dx
         y += dy
         m[y][x] = n
         n += 1
     else:
+        # Implementation of conditions for changing
+        # the directions of moving due to impossibility
+        # of fulfilling the previous conditions.
         if dx == 1:
             dx = 0
             dy = 1
@@ -34,6 +41,6 @@ while n <= 20:
         elif dy == -1:
             dx = 1
             dy = 0
+# Printing of result.
 for i in m:
     print(i)
-
