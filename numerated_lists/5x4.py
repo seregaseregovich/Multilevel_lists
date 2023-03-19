2f7bf316752e2d1b093b3d535d3a55f2c7bb16b4
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-03-19 19:51:14 +0300

    Final revision 1.1. Added by correct description.

--- numerated_lists/5x4.py
+++ numerated_lists/5x4.py
@@ -5,7 +5,7 @@ filled with numbers from 1 to matrix size (A*B). '''
 a = int(input('Enter horizontal matrix size (integer number): '))
 b = int(input('Enter vertical matrix size (integer number): '))
 m = [[0] * a for i in range(b)]
-# Creating initial conditions (position and direction)
+# Stage 2. Creating initial conditions (position and direction)
 # for initial and further enumeration and filling of matrix
 # cells. Initial position is left-upper cell of the matrix.
 x = -1
@@ -13,10 +13,10 @@ y = 0
 dx = 1
 dy = 0
 n = 1
-# Implementation of control over the excess
+# Stage 3. Implementation of control over the excess
 # of the upper value of the matrix content
 while n <= a * b:
-    # Implementation of control over feeling the free cells.
+    # Stage 4. Implementation of control over feeling the free cells.
     # Implementation of control over the formation
     # of correct indexes of matrix cells during the moving.
     if 0 <= x + dx < len(m[0]) and 0 <= y + dy < len(m) \
@@ -26,7 +26,7 @@ while n <= a * b:
         m[y][x] = n
         n += 1
     else:
-        # Implementation of conditions for changing
+        # Stage 5. Implementation of conditions for changing
         # the directions of moving due to impossibility
         # of fulfilling the previous conditions.
         if dx == 1:
